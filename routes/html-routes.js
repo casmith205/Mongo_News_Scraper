60ccc64662ab15075e0221dfdbb7437daddd28df
Carolyn Smith <casmith205@gmail.com>
2018-04-30 13:55:27 -0500

    added basic post route for notes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,14 +8,31 @@ module.exports = function (app) {
     //display home page on inital load
     app.get("/", function (req, res) {
         db.Article.find({})
-        .then(function (result) {
-            var articleObj = {
+            .then(function (result) {
+                var articleObj = {
                     article: result
                 }
-            res.render("home", articleObj);
-        })
-        .catch(function (err) {
-            res.json(err);
-        });
-    })
+                res.render("home", articleObj);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
+
+    // Route for saving/updating an Article's associated Note
+    app.post("/articles/:id", function (req, res) {
+        db.Note.create(req.body)
+            .then(function (dbNote) {
+                return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+            })
+            .then(function (result) {
+                var noteObj = {
+                    note: result
+                }
+                res.render("home", noteObj);
+            })
+            .catch(function (err) {
+                res.json(err);
+            });
+    });
 }
